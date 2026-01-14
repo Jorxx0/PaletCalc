fb4ff6bd2cf699e106a0391c6d49bcfe36b54090
Jorxx04 <jlopezb20@educarex.es>
2026-01-14 21:37:52 +0100

    Initial Commit

---
+++ PaletsCalc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaletsCalc
+//
+//  Created by Jorge López Benito on 14/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaletsCalc/PaletsCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaletsCalcApp.swift
+//  PaletsCalc
+//
+//  Created by Jorge López Benito on 14/1/26.
+//
+
+import SwiftUI
+
+@main
+struct PaletsCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
